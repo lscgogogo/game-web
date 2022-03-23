c8411905a336754485ea5bfae28e818c814fa899
梁书聪 <8948760+liang-shucong@user.noreply.gitee.com>
2022-03-23 15:18:10 +0800

    feat:顶部分享栏

--- 【官网PC】元旦/assets/js/index.js
+++ 【官网PC】元旦/assets/js/index.js
@@ -24,18 +24,75 @@ $(document).ready(function () {
 
   // 左边下载栏
   var flag1 = 2;
-  $(".down-open").click(function(){
+  $(".down-open").click(function () {
     if (flag1 % 2 == 0) {
-        $(".download-wrap").animate({ left: "-174px" });
-        $('.down-open').css("opacity","1")
+      $(".download-wrap").animate({ left: "-174px" });
+      $(".down-open").css("opacity", "1");
     }
     if (flag1 % 2 == 1) {
-        $(".download-wrap").animate({ left: "0px" });
-        $('.down-open').css("opacity","0")
+      $(".download-wrap").animate({ left: "0px" });
+      $(".down-open").css("opacity", "0");
     }
-    flag1++
-  })
-  
+    flag1++;
+  });
+
+  // 顶部分享栏 ???二维码hover效果
+  $(
+    ".share-click,.share-wrap,.community-code,.wechat-code,.taptap-code,.wb-code,.dy-code"
+  ).mouseenter(function () {
+    $(".share-wrap").show();
+  });
+
+  $(".share-click,.share-wrap").mouseleave(function () {
+    $(".share-wrap").hide();
+  });
+
+  $(".community").mouseenter(function () {
+    $(".community-code").show();
+    $(".community").attr("src","./assets/images/share/icon/community-hover.png")
+  });
+  $(".community").mouseleave(function () {
+    $(".community-code").hide();
+    $(".community").attr("src","./assets/images/share/icon/community.png")
+  });
+
+  $(".wechat").mouseenter(function () {
+    $(".wechat-code").show();
+    $(".wechat").attr("src","./assets/images/share/icon/wechat-hover.png")
+  });
+  $(".wechat").mouseleave(function () {
+    $(".wechat-code").hide();
+    $(".wechat").attr("src","./assets/images/share/icon/wechat.png")
+  });
+
+  $(".TapTap").mouseenter(function () {
+    $(".taptap-code").show();
+    $(".TapTap").attr("src","./assets/images/share/icon/TapTap-hover.png")
+  });
+  $(".TapTap").mouseleave(function () {
+    $(".taptap-code").hide();
+    $(".TapTap").attr("src","./assets/images/share/icon/TapTap.png")
+  });
+
+  $(".wb").mouseenter(function () {
+    $(".wb-code").show();
+    $(".wb").attr("src","./assets/images/share/icon/wb-hover.png")
+    
+  });
+  $(".wb").mouseleave(function () {
+    $(".wb-code").hide();
+    $(".wb").attr("src","./assets/images/share/icon/wb.png")
+  });
+
+  $(".dy").mouseenter(function () {
+    $(".dy-code").show();
+    $(".dy").attr("src","./assets/images/share/icon/dy-hover.png")
+  });
+  $(".dy").mouseleave(function () {
+    $(".dy-code").hide();
+    $(".dy").attr("src","./assets/images/share/icon/dy.png")
+  });
+
 
 
 });
